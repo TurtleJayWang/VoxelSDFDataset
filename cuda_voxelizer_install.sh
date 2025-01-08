c66ebd39777b51578e20569b9b606cbab7bf2591
TurtleJayWang <166236546+TurtleJayWang@users.noreply.github.com>
2025-01-08 16:27:55 +0000

    cuda voxelizer install script edited

--- cuda_voxelizer_install.sh
+++ cuda_voxelizer_install.sh
@@ -1,15 +1,9 @@
 cd thirdparty
-# Install Trimesh
-sudo apt update
-sudo apt install mesa-common-dev -y
-sudo apt install libgl1-mesa-dev libglu1-mesa-dev -y
-sudo apt install -y libgl1-mesa-glx
-sudo apt install libxi-dev -y
-cd thirdparty/trimesh2; make
+cd trimesh2; make
 cd ..
 # Install cuda_voxelizer
 mkdir cuda_voxelizer/build
-cd thirdparty/cuda_voxelizer/build
+cd cuda_voxelizer/build
 cmake .. -DTrimesh2_INCLUDE_DIR="../../trimesh2/include" -DTrimesh2_LINK_DIR="../../trimesh2/lib.Linux64"
 cmake --build .
 mv cuda_voxelizer ../cuda_voxelizer
